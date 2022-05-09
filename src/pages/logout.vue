17aa4205c5abc55bf62839e33ec13b15804fb466
Carlos G <cgonz490@stu.southtexascollege.edu>
2022-05-09 11:52:25 -0500

    cosmetic fix

--- src/pages/logout.vue
+++ src/pages/logout.vue
@@ -5,6 +5,6 @@
   </div>
   <div class="flex flex-col w-1/2 mx-auto mt-10 p-4 text-xl font-light bg-blue-700 rounded-lg shadow-md items-center justify-center overflow-hidden">
     <h1 class="text-4xl text-green-300">Logout successful!</h1>
-    <router-link to="/" class="mt-5 text-orange-300 hover:text-orange-300">Log back in 🡢</router-link>
+    <router-link to="/" class="mt-5 text-orange-300 hover:text-orange-500">Log back in 🡢</router-link>
 </div>
 </template>
